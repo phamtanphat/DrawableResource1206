9336fc9f9fe9b234358ffdea7110f090e017ee89
thuviengithub0208 <khoaphp@yahoo.com>
2018-07-14 18:35:51 +0700

    "Clip"

--- app/src/main/java/com/ptp/phamtanphat/drawableresource1206/MainActivity.java
+++ app/src/main/java/com/ptp/phamtanphat/drawableresource1206/MainActivity.java
@@ -1,13 +1,32 @@
 package com.ptp.phamtanphat.drawableresource1206;
 
+import android.graphics.drawable.ClipDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageView imgClip;
+    Button btnClip;
+    ClipDrawable clipDrawable;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imgClip = findViewById(R.id.imageviewDrawableClip);
+        btnClip = findViewById(R.id.buttonDrawableClip);
+
+        clipDrawable = (ClipDrawable) imgClip.getDrawable();
+        clipDrawable.setLevel(1000);
+        btnClip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imgClip.setImageLevel(clipDrawable.getLevel() + 1000);
+            }
+        });
     }
 }
